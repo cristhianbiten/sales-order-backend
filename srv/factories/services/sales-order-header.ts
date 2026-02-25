816754e407f70bff79cf60d6141e05c823707df8
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2026-02-25 17:08:16 -0300

    feat: add salesOrderHeaderController factory and salesOrderHeaderService factory

---
+++ srv/factories/services/sales-order-header.ts
@@ -0,0 +1,13 @@
+import { CustomerRepositoryImpl } from "srv/repositories/customer/implementation";
+import { ProductRepositoryImpl } from "srv/repositories/product/implementation";
+import { SalesOrderHeaderServiceImpl } from "srv/services/sales-order-header/implementation";
+import { SalesOrderHeaderService } from "srv/services/sales-order-header/protocols";
+
+const makeSalesOrderHeaderService = (): SalesOrderHeaderService => {
+    const customerRepository = new CustomerRepositoryImpl();
+    const productRepository = new ProductRepositoryImpl();
+    return new SalesOrderHeaderServiceImpl(productRepository, customerRepository);
+
+}
+
+export const salesOrderHeaderService = makeSalesOrderHeaderService();
