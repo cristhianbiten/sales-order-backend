dc23ebeabc9976ff998acdb36ce27d43d67b3c84
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2026-04-10 15:48:29 -0300

    feat: create findByCustomerId method in SalesReportRepository

--- srv/repositories/sales-report/implementation.ts
+++ srv/repositories/sales-report/implementation.ts
@@ -1,6 +1,6 @@
 import cds from '@sap/cds';
 
-import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReport';
+import { ExpectedResult as SalesReport } from '@models/db/types/SalesReport';
 
 import { SalesReportModel } from '@/models/sales-report';
 import { SalesReportRepository } from '@/repositories/sales-report/protocols';
@@ -12,21 +12,32 @@ export class SalesReportRepositoryImpl implements SalesReportRepository {
         subtratedDays.setDate(subtratedDays.getDate() - days);
         const subtratedDaysISOString = subtratedDays.toISOString();
 
-        const sql = SELECT.from('sales.SalesOrderHeaders')
-            .columns(
-                'id as salesOrderId',
-                'totalAmount as salesOrderTotalAmount',
-                'customer.id as customerId',
-                // eslint-disable-next-line quotes
-                `customer.firstName || ' ' || customer.lastName as customerFullName`
-            )
-            .where({ createdAt: { between: subtratedDaysISOString, and: today } });
-
+        const sql = this.getReportBaseSql().where({ createdAt: { between: subtratedDaysISOString, and: today } });
         const salesReports = await cds.run(sql);
+        return this.mapReportResult(salesReports);
+    }
+
+    public async findByCustomerId(customerId: string): Promise<SalesReportModel[] | null> {
+        const sql = this.getReportBaseSql().where({ customer_id: customerId });
+        const salesReports = await cds.run(sql);
+        return this.mapReportResult(salesReports);
+    }
+
+    private getReportBaseSql(): cds.ql.SELECT<unknown, unknown> {
+        return SELECT.from('sales.SalesOrderHeaders').columns(
+            'id as salesOrderId',
+            'totalAmount as salesOrderTotalAmount',
+            'customer.id as customerId',
+            // eslint-disable-next-line quotes
+            `customer.firstName || ' ' || customer.lastName as customerFullName`
+        );
+    }
+
+    private mapReportResult(salesReports: SalesReport[]): SalesReportModel[] | null {
         if (salesReports.length === 0) {
             return null;
         }
-        return salesReports.map((salesReport: SalesReportByDays) =>
+        return salesReports.map((salesReport: SalesReport) =>
             SalesReportModel.with({
                 salesOrderId: salesReport.salesOrderId as string,
                 salesOrderTotalAmount: salesReport.salesOrderTotalAmount as number,
