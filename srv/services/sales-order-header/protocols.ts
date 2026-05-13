22a5493094ae55d14406bc107d03bb4177eac7c0
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2026-05-13 10:58:52 -0300

    feat: add bulkCreate method in SalesOrderHeaderService

--- srv/services/sales-order-header/protocols.ts
+++ srv/services/sales-order-header/protocols.ts
@@ -1,14 +1,20 @@
 import { User } from '@sap/cds';
 
+import { Payload as BulkCreateSalesOrderPayload } from '@models/db/types/BulkCreateSalesOrder';
+import { CustomerModel } from '@/models/customer';
+import { ProductModel } from '@/models/product';
 import { SalesOrderHeader, SalesOrderHeaders } from '@models/sales';
 
 export type CreationValidationResult = {
     hasErrors: boolean;
     error?: Error;
+    products?: ProductModel[];
+    customer?: CustomerModel;
     totalAmount?: number;
 };
 
 export interface SalesOrderHeaderService {
     beforeCreate(params: SalesOrderHeader): Promise<CreationValidationResult>;
     afterCreate(params: SalesOrderHeaders, loggedUser: User): Promise<void>;
+    bulkCreate(params: BulkCreateSalesOrderPayload[], loggedUser: User): Promise<CreationValidationResult>;
 }
